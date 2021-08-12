ce5491497a27a9c72aff230be8bc834107e6e654
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-08-12 22:14:05 +0200

    fix:try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str % 2 === 0) {
+        return false
+    }
+    let items = str.split('')
+    let len = str.length / 2
+
+    for (let i = 0; i < len; i++) {
+    let first = items[i]
+
+    let last = items[items.length - i - 1]
+
+      if (first == last) {
+      return false
+    }
+  }
+  return true
 }
